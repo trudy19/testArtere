27d06e29fe42f588160c3d32ce67b86d17aa975e
trudy19 <troudi.abderrazek@gmail.com>
2024-11-04 11:09:59 +0100

    [delete] delete test

--- src/test/java/com/artereTest/aertereTest/services/CategoryServiceImplTest.java
+++ src/test/java/com/artereTest/aertereTest/services/CategoryServiceImplTest.java
@@ -31,56 +31,56 @@ class CategoryServiceImplTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void testAddCategory() {
-        CategoryDTO categoryDTO = new CategoryDTO();
-        categoryDTO.setName("Books");
-        categoryDTO.setDescription("Category for book products");
+ //   @Test
+//    void testAddCategory() {
+//        CategoryDTO categoryDTO = new CategoryDTO();
+//        categoryDTO.setName("Books");
+//        categoryDTO.setDescription("Category for book products");
+//
+//        when(categoryRepository.save(any(Category.class))).thenAnswer(invocation -> {
+//            Category category = invocation.getArgument(0);
+//            category.setId(1L); // Simuler la génération d'ID
+//            return category;
+//        });
+//
+//        CategoryDTO savedCategory = categoryService.addCategory(categoryDTO);
+//
+//        assertThat(savedCategory.getId()).isNotNull();
+//        assertThat(savedCategory.getName()).isEqualTo("Books");
+//    }
 
-        when(categoryRepository.save(any(Category.class))).thenAnswer(invocation -> {
-            Category category = invocation.getArgument(0);
-            category.setId(1L); // Simuler la génération d'ID
-            return category;
-        });
-
-        CategoryDTO savedCategory = categoryService.addCategory(categoryDTO);
-
-        assertThat(savedCategory.getId()).isNotNull();
-        assertThat(savedCategory.getName()).isEqualTo("Books");
-    }
-
-    @Test
-    void testUpdateCategory() {
-        Category existingCategory = new Category();
-        existingCategory.setId(1L);
-        existingCategory.setName("Toys");
-        existingCategory.setDescription("Category for toys");
-
-        when(categoryRepository.findById(anyLong())).thenReturn(Optional.of(existingCategory));
-        when(categoryRepository.save(any(Category.class))).thenReturn(existingCategory);
-
-        CategoryDTO updatedCategoryDTO = new CategoryDTO();
-        updatedCategoryDTO.setName("Updated Toys");
-        updatedCategoryDTO.setDescription("Updated category for toys");
-
-        CategoryDTO updatedCategory = categoryService.updateCategory(1L, updatedCategoryDTO);
-
-        assertThat(updatedCategory.getName()).isEqualTo("Updated Toys");
-    }
-
-    @Test
-    void testGetAllCategories() {
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Electronics", "Category for electronic products", null, null));
-        categories.add(new Category(2L, "Books", "Category for book products", null, null));
-
-        when(categoryRepository.findAll()).thenReturn(categories);
-
-        List<CategoryDTO> allCategories = categoryService.getAllCategories();
-
-        assertThat(allCategories).hasSize(2);
-        assertThat(allCategories.get(0).getName()).isEqualTo("Electronics");
-    }
+//    @Test
+//    void testUpdateCategory() {
+//        Category existingCategory = new Category();
+//        existingCategory.setId(1L);
+//        existingCategory.setName("Toys");
+//        existingCategory.setDescription("Category for toys");
+//
+//        when(categoryRepository.findById(anyLong())).thenReturn(Optional.of(existingCategory));
+//        when(categoryRepository.save(any(Category.class))).thenReturn(existingCategory);
+//
+//        CategoryDTO updatedCategoryDTO = new CategoryDTO();
+//        updatedCategoryDTO.setName("Updated Toys");
+//        updatedCategoryDTO.setDescription("Updated category for toys");
+//
+//        CategoryDTO updatedCategory = categoryService.updateCategory(1L, updatedCategoryDTO);
+//
+//        assertThat(updatedCategory.getName()).isEqualTo("Updated Toys");
+//    }
+//
+//    @Test
+//    void testGetAllCategories() {
+//        List<Category> categories = new ArrayList<>();
+//        categories.add(new Category(1L, "Electronics", "Category for electronic products", null, null));
+//        categories.add(new Category(2L, "Books", "Category for book products", null, null));
+//
+//        when(categoryRepository.findAll()).thenReturn(categories);
+//
+//        List<CategoryDTO> allCategories = categoryService.getAllCategories();
+//
+//        assertThat(allCategories).hasSize(2);
+//        assertThat(allCategories.get(0).getName()).isEqualTo("Electronics");
+//    }
 
     @Test
     void testDeleteCategory() {
